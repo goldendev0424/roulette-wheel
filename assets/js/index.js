0f046d805f0fa474019ecf5bcefabba0081e3d16
goldendev0424 <golden70044@gmail.com>
2021-04-21 13:10:24 +1000

    Complete

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,4 @@
-const ObservableObj = new Observable();
-
-var currentBallRotation = 0;
-var currentWheelRotation = 0;
-var currentWheelIndex = 0;
-var isRotating = false;
+let isRotating = false;
 const rouletteWheelNumbers = [
   0,
   32,
@@ -44,170 +39,99 @@ const rouletteWheelNumbers = [
   26
 ];
 
-const getRouletteWheelNumber = index => {
-  const rouletteCount = rouletteWheelNumbers.length;
+document.querySelector('#btn_start').addEventListener('click', () => {
+  wheelStart();
+})
 
-  let i = index >= 0 ? index % 37 : 37 - Math.abs(index % 37);
-  i = i >= rouletteCount ? i % rouletteCount : i;
+document.querySelector('#btn_stop').addEventListener('click', () => {
+  gBackAnimation.reset();
+  gBallAnimation.reset();
 
-  return rouletteWheelNumbers[i];
-};
+  let index = Math.floor(Math.random() * (36 - 0 + 1) + 0);
+  const resultNumber = rouletteWheelNumbers[index]
+  console.log('resultNumber:', resultNumber)
+  wheelStop(resultNumber);
+})
 
-const getRouletteWheelColor = index => {
-  const rouletteCount = rouletteWheelNumbers.length;
-
-  let i = index >= 0 ? index % 37 : 37 - Math.abs(index % 37);
-  i = i >= rouletteCount ? i % rouletteCount : i;
-  
-  return i == 0 ? "green" : i % 2 == 0 ? "black" : "red";
-};
-
-window.rouletteWheelNumbers = rouletteWheelNumbers;
-
-function addFlipper() {
-  const mkDiv = className => {
-    const d = document.createElement("div");
-    d.classList.add(...className.split(" "));
-    return d;
-  };
-  const flipper = mkDiv("flipper");
-  const front = mkDiv("front-face");
-  const back = mkDiv("back-face");
-  flipper.appendChild(front);
-  flipper.appendChild(back);
-  document.querySelector(".result").appendChild(flipper);
-  return (number, color) => {
-    flipper.classList.add("flip", color);
-    back.innerText = number;
-  };
-}
-
-function startRotation(speed) {
+// Wheel start
+function wheelStart() {
   if (isRotating) {
     return;
   }
 
   isRotating = true;
 
-  const writeResult = addFlipper();
-
-  const bezier = [0.165, 0.84, 0.44, 1.005];
-
-  const newWheelIndex = currentWheelIndex - speed;
-  const result = getRouletteWheelNumber(newWheelIndex);
-  const resultColor = getRouletteWheelColor(newWheelIndex);
-  (() => {
-    const newRotaion = currentWheelRotation + (360 / 37) * speed;
-    var myAnimation = anime({
-      targets: [".layer-2", ".layer-4"],
-      rotate: function () {
-        return newRotaion;
-      },
-      duration: function () {
-        return 5000;
-      },
-      loop: 1,
-      easing: `cubicBezier(${bezier.join(",")})`,
-      complete: (...args) => {
-        currentWheelRotation = newRotaion;
-        currentWheelIndex = newWheelIndex;
-      }
-    });
-  })();
-
-  (() => {
-    const newRotaion = -4 * 360 + currentBallRotation;
-    let translateY = [
-      { value: 0, duration: 2000 },
-      { value: 10, duration: 1000 },
-      { value: 15, duration: 900 },
-      { value: 50, duration: 1000 }
-    ];
-    
-    if(window.innerWidth < 485) {
-      translateY[0].value = 0;
-      translateY[1].value = 5;
-      translateY[2].value = 5;
-      translateY[3].value = 30;
-    }
-    
-    var myAnimation1 = anime({
-      targets: ".ball-container",
-      translateY,
-      rotate: [{ value: newRotaion, duration: 4000 }],
-      duration: function () {
-        return 4000; // anime.random(800, 1400);
-      },
-      loop: 1,
-      easing: `cubicBezier(${bezier.join(",")})`,
-      complete: () => {
-        currentBallRotation = newRotaion;
-        writeResult(result, resultColor);
-        isRotating = false;
-      }
-    });
-  })();
-}
-
-function offsetEl(el) {
-  var rect = el.getBoundingClientRect(),
-    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  return {
-    top: rect.top + scrollTop,
-    left: rect.left + scrollLeft,
-    width: rect.width,
-    height: rect.height
-  };
-}
-
-function isInRadiusEl(el, x, y) {
-  const o = offsetEl(el);
-  const cx = o.left + o.width / 2;
-  const cy = o.top + o.height / 2;
-  const dx = x - cx;
-  const dy = y - cy;
-  const r = o.width / 2;
-  return Math.pow(dx, 2) + Math.pow(dy, 2) <= Math.pow(r, 2);
-}
-
-const documentEvent = eventName =>
-  ObservableObj.pipe(
-    ObservableObj.fromEvent(document, eventName),
-    ObservableObj.map(e =>
-      e.type == "touchstart" || e.type == "touchmove"
-        ? { x: e.touches[0].clientX, y: e.touches[0].clientY }
-        : { x: e.clientX, y: e.clientY }
-    )
-  );
-
-const tryRotate = ([p0, p1]) => {
-  if(!p0 || !p1) {
+  if(window.gBackAnimation) {
+    gBackAnimation.play();
+    gBallAnimation.play();
     return;
   }
 
-  const w = document.querySelector(".layer-2.wheel");
-  if (isInRadiusEl(w, p0.x, p0.y)) {
-    const d = Math.round(
-      Math.sqrt(Math.pow(p0.x - p1.x, 2) + Math.pow(p0.y - p1.y, 2)) / 4
-    );
-    if (Math.abs(d) > 3) {
-      window.startRotation(d);
+  var backAnimation = anime({
+    targets: ['.layer-2', '.layer-4'],
+    rotate: 360,
+    easing: 'linear',
+    loop: true,
+  });
+
+  var ballAnimation = anime({
+    targets: ['.ball-container'],
+    rotate: 360,
+    easing: 'linear',
+    loop: true,
+    direction: 'reverse',
+  });
+
+  window.gBackAnimation = backAnimation;
+  window.gBallAnimation = ballAnimation;
+}
+
+/**
+ * Wheel stop function
+ * 
+ * @param number resultNumber winning betting number
+ */ 
+function wheelStop(resultNumber) {
+  const bezier = [0.165, 0.84, 0.44, 1.005];
+  
+  // wheel animation
+  let i = rouletteWheelNumbers.findIndex((item) => {
+    return item === resultNumber
+  });
+
+  let newRotaion = 360 - (360 / rouletteWheelNumbers.length) * i
+  newRotaion = newRotaion < 180 ? newRotaion + 360 : newRotaion;
+
+  anime({
+    targets: [".layer-2", ".layer-4"],
+    rotate: newRotaion,
+    duration: 4000,
+    loop: 1,
+    easing: `cubicBezier(${bezier.join(",")})`,
+    complete: () => {
     }
+  });
+
+  // Ball animation
+  let translateY = [
+    { value: 0, duration: 2000 },
+    { value: 0, duration: 1000 },
+    { value: 0, duration: 1000 },
+    { value: 50, duration: 1000 }
+  ];
+  
+  if(window.innerWidth < 485) {
+    translateY[3].value = 30;
   }
-};
-
-ObservableObj.zip(documentEvent("mousedown"))(documentEvent("mouseup")).subscribe({
-  next: tryRotate
-});
-
-ObservableObj.zip(documentEvent("touchstart"))(
-  ObservableObj.pipe(
-    ObservableObj.withLatestFrom(documentEvent("touchmove"))(ObservableObj.fromEvent(document, "touchend")),
-    ObservableObj.map(([_, r]) => r)
-  )
-).subscribe({
-  next: tryRotate
-});
-
-window.startRotation = startRotation;
+    
+  anime({
+    targets: '.ball-container',
+    translateY,
+    rotate: [{ value: 0, duration: 4000 }],
+    loop: 1,
+    easing: `cubicBezier(${bezier.join(",")})`,
+    complete: () => {
+      isRotating = false;
+    }
+  });
+}
